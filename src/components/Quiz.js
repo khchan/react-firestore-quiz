dd40c3de766e66c86d0e2ccb6e2268c7f6f85fc2
David Yu <lewen.yu@gmail.com>
2019-08-21 22:17:55 -0700

    allow you to reselect a different answer
    but don't allow you to select in the results stage

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -133,14 +133,9 @@ class Quiz extends React.Component {
                now > end;
     }
 
-    selectAnswer(idx) {
-        // you've already selected an answer
-        if (this.state.selectedAnswerIdx !== -1) {
-            return;
-        }
-
+    selectAnswer(idx, questionStage) {
         // you can no longer select an answer
-        if (this.isOutOfTime()) {
+        if (this.isOutOfTime() || questionStage === constants.QuestionStage.READ_RESULTS) {
             return;
         }
 
@@ -201,24 +196,26 @@ class Quiz extends React.Component {
       );
     }
 
-    renderAnswer(a, idx, animate) {
-        const buttonAnimate = animate ? '-quiz-answer -intro -seq' + idx : '-quiz-answer';
+    renderAnswer(a, idx, questionStage) {
+        const buttonAnimate = questionStage == constants.QuestionStage.AUDIENCE_ANSWER
+                            ? '-quiz-answer -intro -seq' + idx : '-quiz-answer';
         const isAnswerSelected = this.state.selectedAnswerIdx == idx ? '-selected' : '';
         return (
           <button className={`button ${buttonAnimate} ${isAnswerSelected}`}
-                  onClick={() => this.selectAnswer(idx)}>{a}</button>
+                  onClick={() => this.selectAnswer(idx, questionStage)}>{a}</button>
         );
     }
 
-    renderAnswers(answers, showResults) {
+    renderAnswers(answers, questionStage) {
         if (!answers || answers.length === 0) {
             return null;
         }
+        const showResults = questionStage == constants.QuestionStage.READ_RESULTS;
         let rendered = [];
         for (let i = 0; i < answers.length; i++) {
             rendered.push(
                 <div className="grid-item" key={i}>
-                    {this.renderAnswer(answers[i], i, /* animate in */ !showResults)}
+                    {this.renderAnswer(answers[i], i, questionStage)}
                     {showResults ? this.renderProfilesForResult(i) : null}
                 </div>
             );
@@ -279,7 +276,7 @@ class Quiz extends React.Component {
                         <p className="title-text">Question {wordify(this.state.currentQuestionId)}</p>
                         <p className="question-text">{questionText}</p>
                         {questionImg}
-                        {this.renderAnswers(answers, /* show results */ false)}
+                        {this.renderAnswers(answers, this.state.questionStage)}
                     </div>
                 );
             case constants.QuestionStage.READ_RESULTS:
@@ -295,7 +292,7 @@ class Quiz extends React.Component {
                         <p className="title-text">Results</p>
                         <p className="results-question-text">{questionText}</p>
                         {questionImgReveal}
-                        {this.renderAnswers(answers, /* show results */ true)}
+                        {this.renderAnswers(answers, this.state.questionStage)}
                     </div>
                 );
             default:
